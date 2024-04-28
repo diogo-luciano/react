ebaa7fb9f91251645957bed3f3c8bbf9efcd3d49
Diogo <diogo@gmail.com>
2024-04-28 21:05:41 +0100

    Feedback App main func finished

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,28 +1,50 @@
-import propTypes from "prop-types"
-import FeedbackItem from "./FeedbackItem"
+import { motion, AnimatePresence } from 'framer-motion';
+import propTypes from 'prop-types';
+import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({feedback, handleDelete}) {
-    if(!feedback || feedback.length === 0) {
-        return <p>No Feedback Yet</p>
-    }
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
 
-    return (
-        <div className="feedback-list">
-            {feedback.map((item) =>
-                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-            )}
-        </div>
-    )
+  return (
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
+
+  /* return (
+    <div className='feedback-list'>
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  ); */
 }
 
 FeedbackList.propTypes = {
-    feedback: propTypes.arrayOf(
-        propTypes.shape({
-            id: propTypes.number.isRequired,
-            text: propTypes.string.isRequired,
-            rating: propTypes.number.isRequired
-        })
-    )
-}
+  feedback: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.number.isRequired,
+      text: propTypes.string.isRequired,
+      rating: propTypes.number.isRequired,
+    })
+  ),
+};
 
-export default FeedbackList
+export default FeedbackList;
